69505dbe549dffc9f1b39066ad3c6c0ec5fe84d7
Karun <karun012@gmail.com>
2014-05-25 16:03:48 -0700

    Initial implementation. Lots of cleanup left

--- src/Shadower.hs
+++ src/Shadower.hs
@@ -1,2 +1,31 @@
-module Shadower where
+{-# OPTIONS -Wall #-}
+ 
+module Main where
 
+import Filesystem.Path.CurrentOS (fromText, encodeString)
+import Data.Text (pack)
+import Control.Monad (void)
+import System.FSNotify
+import System.Environment
+import System.Cmd
+import System.Exit
+
+main :: IO ()
+main = getArgs >>= getFilePathFromArgs >>= run
+
+getFilePathFromArgs ::  [String] -> IO FilePath
+getFilePathFromArgs []       = return "."
+getFilePathFromArgs (filePath:_) = return filePath
+
+run :: FilePath -> IO ()
+run path = withManager $ \m -> do
+       _ <- watchTree m (fromText $ pack path) (const True) handler
+       _ <- getLine
+       exitSuccess
+
+handler :: Event -> IO()
+handler action = case action of 
+                     Modified file _ -> runDocTests (encodeString file)
+                     _ -> return ()
+runDocTests :: String -> IO ()
+runDocTests file = void $ system $ "doctest -isrc " ++ file
